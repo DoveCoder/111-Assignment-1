f39bdcccef8269cdd729d986244704a5358eb847
Julian Smith <jmsmith118@yahoo.com>
2021-11-15 12:20:55 -0800

    Added hello world logic

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+"""Sample hello world Flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello, World!<h1>"
---
+++ run.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+"""Init Script for app"""
+
+from app import app
